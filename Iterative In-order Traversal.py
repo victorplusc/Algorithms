52d73c387180e8719799f72368037d17301bb3f9
Victor <24923454+VictorPlusC@users.noreply.github.com>
2018-11-25 16:20:16 -0500

    Add files via upload

---
+++ Iterative In-order Traversal.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+#Iterative in-order traversal
+def inorderTraversal(self, root):
+
+    stack = []
+        
+    while True:
+        if root is not None:
+            stack.append(root)
+            root = root.left
+        else:
+            if stack != []:
+                root = stack.pop()
+                visit(root)
+                root = root.right
+            else:
+                break
+
+"""
+#A placeholder function for any 'action' applied during the traversals
+"""
+def visit(node):
+    pass
