b443287abd79fb2eb9b20587628979c863224f22
Victor <24923454+VictorPlusC@users.noreply.github.com>
2018-12-02 13:28:48 -0500

    Update and rename 350. Intersection of Two Arrays II to 350. Intersection of Two Arrays II.py

--- Leetcode/350. Intersection of Two Arrays II.py
+++ Leetcode/350. Intersection of Two Arrays II.py
@@ -35,3 +35,38 @@ class Solution:
                 intersection.append(item)
         
         return intersection
+
+"""
+If sorted:
+"""
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        
+        i = 0
+        j = 0
+        
+        intersection
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                intersection.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i] < nums2[j]:
+                i+= 1
+            else:
+                j+= 1
+        
+        return intersection
