ae7ba993f16040e643309d0a6644814546db3dbe
Victor <24923454+VictorPlusC@users.noreply.github.com>
2019-03-11 20:08:27 -0400

    Update 965. Univalued Binary Tree.py

--- Leetcode/965. Univalued Binary Tree.py
+++ Leetcode/965. Univalued Binary Tree.py
@@ -1,3 +1,9 @@
+"""
+A binary tree is univalued if every node in the tree has the same value.
+
+Return true if and only if the given tree is univalued.
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
