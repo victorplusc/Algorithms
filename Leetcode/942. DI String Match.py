a54992e9c6eb8b769236ebca3f86e9d1a947c74a
Victor <career@victorplusc.com>
2019-05-08 00:15:08 -0400

    Update 942. DI String Match.py

--- Leetcode/942. DI String Match.py
+++ Leetcode/942. DI String Match.py
@@ -1,3 +1,5 @@
+# 942. DI String Match
+
 class Solution:
     def diStringMatch(self, S: str) -> List[int]:
         
@@ -7,19 +9,11 @@ class Solution:
         
         for i, char in enumerate(S):
             if char == "I":
-                if arr == []:
-                    arr.append(0)
-                    left += 1
-                else:
-                    arr.append(vals[left])
-                    left += 1
+                arr.append(vals[left])
+                left += 1
             if char == "D":
-                if arr == []:
-                    arr.append(vals[-1])
-                    right += 1
-                else:
-                    arr.append(vals[-1-right])
-                    right += 1
+                arr.append(vals[-1-right])
+                right += 1
         
         if S[-1] == "I":
             arr.append(vals[left])
