f0e494ea5438d677f27b5a5c7660ac02fc510466
Victor <career@victorplusc.com>
2019-07-15 22:06:47 -0400

    Create 46. Permutations.py

---
+++ Leetcode/46. Permutations.py
@@ -0,0 +1,26 @@
+"""
+46. Permutations
+Given a collection of distinct integers, return all possible permutations.
+"""
+# Time complexity: O(N*N)
+# Space complexity: O(N*N)
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutations = []
+        self.recursive_helper(permutations, nums, [])
+        return permutations
+    
+    def recursive_helper(self, permutations, nums, current):
+        if nums == []:
+            print(current)
+            permutations.append(current)
+        else:
+            for i in range(len(nums)):
+                current_num = nums[i]
+                current.append(current_num)
+                nums.remove(current_num)
+                self.recursive_helper(permutations, nums, current)
+                nums.insert(i, current_num)
+                current.remove(current_num)
+    
