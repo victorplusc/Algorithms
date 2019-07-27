64023e5df984ba916f8112c6320d953d5394c377
Victor <career@victorplusc.com>
2019-07-27 14:26:20 -0400

    Update h_index.py

--- Elements of Programming Interviews/h_index.py
+++ Elements of Programming Interviews/h_index.py
@@ -2,11 +2,21 @@
 The h-index is the largest number h such that there are h elements in the array that are of at least size h.
 """
 
-##### Efficient algorithm
+##### EFFICIENT SOLUTION
 
+# Time complexity: O(N log N)
+# Space complexity: O(N)
 
-
-
+def find_h_index(publications: [int]) -> int:
+    publications.sort()
+    h_index = 0
+    length = len(publications)
+    
+    for i, val in enumerate(publications):
+        if val > h_index and length-i >= val:
+            h_index = val
+            
+    return h_index
 
 
 
