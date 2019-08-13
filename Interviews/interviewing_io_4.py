5163aa77ce1211fadd6050bab0b81d29c9950acc
Victor <career@victorplusc.com>
2019-08-12 20:27:26 -0400

    Create interviewing_io_4.py

---
+++ Interviews/interviewing_io_4.py
@@ -0,0 +1,105 @@
+# Let Operation be some struct with fields http_method and data:
+# struct Operation {
+#   http_method: HTTPMethod,
+#   Data: String
+# }
+
+# and HTTPMethod some enumeration of valid HTTP verbs:
+# enum HTTPMethod {
+#   Get,
+#   Post,
+#   Put,
+#   Delete,
+# }
+
+# Write a function called process_in_order() that takes a vector of operations and returns a vector of results. The order of results should correspond to the order of the input operations (i.e. the first result element should be the response to the first operation), but the operations should be processed in a different order, specifically in order of the method as follows:
+
+# 1)DELETE
+# 2)POST
+# 3)PUT
+# 4)GET
+
+# Assume you can call process_operation() on a single operation to get back a single result. You should not have to implement a sorting algorithm.
+
+# For example, if I receive
+# [{ GET, "a" }, { POST, "ab" }, { POST, "abb"} ,{ PUT, "abc" }]
+# I want to process it in the order
+# [{POST, "abb"}{ POST, "ab" }, { PUT, "abc" }, { GET, "a" }]
+
+# Let’s say procees_operation() just gives you the length of the input string, then we’ll get 
+# process_operation({POST, "ab"}) => 2
+# process_operation({PUT, "abc"}) => 3
+# process_operation({GET, "a"}) => 1
+
+# So we want process_in_order() to return
+# [3, 1, 2]
+
+"""
+In the case of one big list:
+
+[ ["DELETE", ... ], ["POST", ... ] ]
+
+for array in arrays:
+    unpack(array)
+
+for unpacking:
+    1) input: [1,2,3,4]
+    2) output: 1, 2, 3, 4
+
+"""
+
+# Time complexity: O(N)
+# Space complexity: O(N)
+
+def process_in_order(operations: [tuple]) -> [int]:
+    delete_list = []
+    post_list = []
+    put_list = []
+    get_list = []
+    positions = position_indices = {}
+    
+    """
+    [ positions[processed_operations[1]], ...
+    """
+    
+    for i, op in enumerate(operations):
+        position_indices[i] = op
+        positions[op] = i
+        
+        if "DELETE" in op:
+            delete_list.append(op)
+        elif "POST" in op:
+            post_list.append(op)
+        elif "PUT" in op:
+            put_list.append(op)
+        else:
+            get_list.append(op)
+    
+    # processed_operations = []
+    
+    for op_list in [delete_list, post_list, put_list, get_list]:
+        for op in (op_list):
+            position_indices[positions[op]] = process_operation(op)
+            #processed_operations.append((positions[op], process_operation(op)))
+            
+    #processed_operations.sort()
+    
+    #return [op[1] for op in processed_operations]
+    
+    processed_operations = []
+    for i in range(len(operations)):
+        processed_operations.append(position_indices[i])
+    
+    return processed_operations
+    
+            
+# For example, if I receive
+# [{ GET, "a" }, { POST, "ab" }, { POST, "abb"} ,{ PUT, "abc" }]
+# I want to process it in the order
+# [{POST, "abb"}{ POST, "ab" }, { PUT, "abc" }, { GET, "a" }] 
+            
+def process_operation(operation):
+    return len(operation[1])
+            
+           
+print(process_in_order([("GET", "a"), ("POST", "ab"), ("DELETE", "abba"), ("POST", "abb"), ("PUT", "abc")]))
