bfd947f2da6ae4082c355d76ef0be82fcc848caf
Victor <career@victorplusc.com>
2019-08-13 23:41:47 -0400

    Update 1087. Brace Expansion.py

--- Leetcode/1087. Brace Expansion.py
+++ Leetcode/1087. Brace Expansion.py
@@ -1,34 +1,40 @@
+"""
+1087. Brace Expansion
+A string S represents a list of words.
+
+Each letter in the word has 1 or more options.  If there is one option, the letter is represented as is.  If there is more than one option, then curly braces delimit the options.  For example, "{a,b,c}" represents options ["a", "b", "c"].
+
+For example, "{a,b,c}d{e,f}" represents the list ["ade", "adf", "bde", "bdf", "cde", "cdf"].
+
+Return all words that can be formed in this manner, in lexicographical order.
+"""
+
 class Solution:
-    def expand(self, S: str) -> List[str]:
+    def expand(self, s: str) -> List[str]:
         results = []
-        
-        char_count = 0
-        inside_brackets = False
-        bracket_index = {}
-        new_str = []
-        for i, c in enumerate(S):
-            
-            if c == "{":
-                bracket_index[char_count] = []
-                new_str.append("*")
-                inside_brackets = True
-            
-            if not inside_brackets:
-                char_count += 1
-                new_str.append(c)
-            
-            if c == "}" and inside_brackets:
-                char_count += 1
-                inside_brackets = False
-                
-            if c not in {"{", "}", ","} and inside_brackets:
-                bracket_index[char_count].append(c)
-                
-            
-        #formatted_str = "".join(new_str)
-        print(bracket_index)
-        print(char_count)
-                
-        self.expand_helper(new_str, [], results, bracket_index)
-        print(new_str)
+        dfs(s, 0, [], results)
         return results
+    
+def dfs(s: str, index: int, curr: [], results: List[int]):
+    if index == len(s):
+        results.append("".join(curr))
+        return
+
+    c = s[index]
+
+    if c == "{":
+        char_list = []
+        end_index = index + 1
+        while end_index < len(s) and s[end_index] != "}":
+            if s[end_index] != ",":
+                char_list.append(s[end_index])
+            end_index += 1
+
+        char_list.sort()
+        for x in char_list:
+            curr.append(x)
+            dfs(s, end_index + 1, [_ for _ in curr], results)
+        
+    elif c not in {",", "}"}:
+        curr.append(c)
+        dfs(s, index + 1, [_ for _ in curr], results)
