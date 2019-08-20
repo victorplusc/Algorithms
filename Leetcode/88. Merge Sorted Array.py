a79f23573b23dfc63c9d34fcbe1e3251db4bb86b
Victor <career@victorplusc.com>
2019-08-19 20:31:28 -0400

    Update 88. Merge Sorted Array.py

--- Leetcode/88. Merge Sorted Array.py
+++ Leetcode/88. Merge Sorted Array.py
@@ -15,9 +15,29 @@ nums2 = [2,5,6],       n = 3
 Output: [1,2,2,3,5,6]
 """
 
+# Time complexity: O(M+N)
+# Space complexity: O(1)
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
         
+        i = m - 1
+        j = n - 1
+        offset = m+n - 1
+        
+        while i >= 0 and j >= 0:
+            if nums1[i] < nums2[j]:
+                nums1[offset] = nums2[j]
+                j -= 1
+            else:
+                nums1[offset] = nums1[i]
+                i -= 1
+            offset -= 1
+            
+        k = 0
+        while k <= j:
+            nums1[k] = nums2[k]
+            k+=1
