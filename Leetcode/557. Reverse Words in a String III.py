a81fbee1c0bd9290c557c9c0d834105a271ccedc
Victor <career@victorplusc.com>
2019-08-24 21:37:03 -0400

    Update 557. Reverse Words in a String III.py

--- Leetcode/557. Reverse Words in a String III.py
+++ Leetcode/557. Reverse Words in a String III.py
@@ -6,20 +6,27 @@ within a sentence while still preserving whitespace and initial word order.
     #Input: "Let's take LeetCode contest"
     #Output: "s'teL ekat edoCteeL tsetnoc"
 
-def reverseWords(s: "String to be reversed"):
-    temp = s.split()
-    for i in range(len(temp)):
-        if i == len(temp)-1:
-            temp[i] = reverseWord(temp[i])
-        else:
-            temp[i] = reverseWord(temp[i]) + " "
-    return "".join(temp)
-
-def reverseWord(word: "String"):
-    temp = []
-    for i in range(len(word)):
-        temp.append(word[len(word)-i-1])
-    return "".join(temp)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = list(s)
+        start = end = 0
+        
+        while start < len(s):
+            if end+1 == len(s):
+                self.reverse_word(words, start, end)
+                break
+            elif words[end] == " ":
+                self.reverse_word(words, start, end-1)
+                start = end + 1
+            end += 1
+                
+        return "".join(words)
+        
+    def reverse_word(self, s, start, end):
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
 
 #Time complexity: O(N)
-#Space complexity: O(N)
+#Space complexity: O(1), if in a language where the string is mutable, else O(N)
