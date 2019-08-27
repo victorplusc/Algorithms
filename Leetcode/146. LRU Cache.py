bed9d7bcacf3c7550ddf0e80133cd3c13295eba9
Victor <career@victorplusc.com>
2019-08-26 20:58:24 -0400

    Update 146. LRU Cache.py

--- Leetcode/146. LRU Cache.py
+++ Leetcode/146. LRU Cache.py
@@ -25,16 +25,71 @@ cache.get(3);       // returns 3
 cache.get(4);       // returns 4
 """
 
+class DLinkedNode():
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = 0
+        self.next = 0
+
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cache = {}
+        self.size = 0
+        self.capacity = capacity
+        self.head, self.tail = DLinkedNode(), DLinkedNode()
         
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        
+        node = self.cache.get(key, None)
+        if not node:
+            return -1
+        self._move_to_head(node)
+        return node.value
 
-    def put(self, key: int, value: int) -> None:
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+    
+    def _remove_node(self, node):
+        prev = node.prev
+        new = node.next
+        prev.next = new
+        new.prev = prev
+    
+    def _add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next
         
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def _pop_tail(self):
+        last = self.tail.prev
+        self._remove_node(last)
+        return last
+    
+    def put(self, key: int, value: int) -> None:
+        node = self.cache.get(key)
+        if not node:
+            new_node = DLinkedNode()
+            new_node.key = key
+            new_node.value = value
+            
+            self.cache[key] = new_node
+            self._add_node(new_node)
+            self.size += 1
+            
+            if self.size > self.capacity:
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
+        else:
+            node.value = value
+            self._move_to_head(node)
 
 
 # Your LRUCache object will be instantiated and called as such:
