966fde2daf32da703a72e5c94cb1ae55fbd6f203
Victor <career@victorplusc.com>
2019-08-30 23:01:12 -0400

    Create 200. Number of Islands.py

---
+++ Leetcode/200. Number of Islands.py
@@ -0,0 +1,28 @@
+"""
+200. Number of Islands
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+"""
+
+# Time complexity: O(N*M)
+# Space complexity: O(N*M)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        islands = 0
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == "1":
+                    self.dfs(grid, y, x)
+                    islands += 1
+        return islands
+    
+    def dfs(self, grid, y, x):
+        if y < 0 or x < 0 or y >= len(grid) or x >= len(grid[0]) or grid[y][x] != "1":
+            return
+        grid[y][x] = "X"
+        self.dfs(grid, y+1, x)
+        self.dfs(grid, y-1, x)
+        self.dfs(grid, y, x+1)
+        self.dfs(grid, y, x-1)
