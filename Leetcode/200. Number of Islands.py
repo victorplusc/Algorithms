5b7db8945cd48fcbaabf089607dbed02d53fcbde
Victor <career@victorplusc.com>
2019-08-31 11:35:50 -0400

    Update 200. Number of Islands.py

--- Leetcode/200. Number of Islands.py
+++ Leetcode/200. Number of Islands.py
@@ -26,3 +26,46 @@ class Solution:
         self.dfs(grid, y-1, x)
         self.dfs(grid, y, x+1)
         self.dfs(grid, y, x-1)
+        
+        
+# ================================== FINDING MAXIMUM ISLAND SIZE (VARIANT) ==================================
+
+# Time complexity: O(N*M)
+# Space complexity: O(N*M)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        size = 0
+        islands = 0
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == "1":
+                    size = max(self.dfs(grid, y, x), size)
+                    islands += 1
+        
+        return islands
+    
+    def dfs(self, grid, y, x):
+        if y < 0 or x < 0 or y >= len(grid) or x >= len(grid[0]) or grid[y][x] != "1":
+            return
+        if grid[y][x] == "1":
+            size = 1
+        else:
+            size = 0
+        grid[y][x] = "X"
+        y1_size = self.dfs(grid, y+1, x)
+        y2_size = self.dfs(grid, y-1, x)
+        x1_size = self.dfs(grid, y, x+1)
+        x2_size = self.dfs(grid, y, x-1)
+        
+        if y1_size:
+            size += y1_size
+        if y2_size:
+            size += y2_size
+        if x1_size:
+            size += x1_size
+        if x2_size:
+            size += x2_size
+    
+        return size
