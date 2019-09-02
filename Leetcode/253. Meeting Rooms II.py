41df1e842480a99be5c231323c72e3a62e80dd28
Victor <career@victorplusc.com>
2019-09-01 20:32:02 -0400

    Create 253. Meeting Rooms II.py

---
+++ Leetcode/253. Meeting Rooms II.py
@@ -0,0 +1,69 @@
+"""
+253. Meeting Rooms II
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+"""
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        #return self.heap_min_meeting_rooms(intervals)
+        return self.ordering_min_meeting_rooms(intervals)
+    
+# Heap method
+# Time complexiy: O(N log N)
+# Space complexity: O(N)
+    def heap_min_meeting_rooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        if not intervals:
+            return 0
+        
+        free_rooms = []
+        intervals.sort(key= lambda x: x[0])
+        heapq.heappush(free_rooms, intervals[0][1])
+
+        for i, interval in enumerate(intervals[1:]):
+            if free_rooms[0] <= interval[0]:
+                heapq.heappop(free_rooms)
+            
+            heapq.heappush(free_rooms, interval[1])
+            
+        return len(free_rooms)
+
+    
+    
+# Chronological ordering method
+# Time complexiy: O(N log N)
+# Space complexity: O(N)
+    def ordering_min_meeting_rooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        if not intervals:
+            return 0
+        
+        start_times = sorted([interval[0] for interval in intervals])
+        end_times = sorted([interval[1] for interval in intervals])
+        
+        used_rooms = 0
+        start_pointer = end_pointer = 0
+        
+        while start_pointer < len(intervals):
+            if start_times[start_pointer] >= end_times[end_pointer]:
+                used_rooms -= 1
+                end_pointer += 1
+            used_rooms += 1
+            start_pointer += 1
+            
+        return used_rooms
