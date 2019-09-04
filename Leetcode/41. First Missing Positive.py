6775c21b5a679ff2fb8b89c9123600751d54ed68
Victor <career@victorplusc.com>
2019-09-04 19:49:02 -0400

    Update 41. First Missing Positive.py

--- Leetcode/41. First Missing Positive.py
+++ Leetcode/41. First Missing Positive.py
@@ -1,17 +1,64 @@
-#41. First Missing Positive
 """
+41. First Missing Positive
 Given an unsorted integer array, find the smallest missing positive integer.
 
-1 is considered the lowest positive integer.
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
 """
 
+# Time complexity: O(N)
+# Space complexity: O(1)
 class Solution:
     def firstMissingPositive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        numSet = {i for i in nums if i >= 1}
+        # num_set = {i for i in nums if i >= 1}
+        # for i in range(1, len(num_set)+1):
+        #     if i not in num_set: return i
         
-        for i in range(1, len(numSet)+2):
-            if i not in numSet: return i
+        if 1 not in nums:
+            return 1
+        
+        n = len(nums)
+        if n == 1:
+            return 2
+        
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = 1
+        
+        # Use index as a hash key and number sign as a presence detector.
+        # For example, if nums[1] is negative that means that number `1`
+        # is present in the array. 
+        # If nums[2] is positive - number 2 is missing.
+        for i in range(n): 
+            a = abs(nums[i])
+            # If you meet number a in the array - change the a-th element to a negative.
+            if a == n:
+                nums[0] = - abs(nums[0])
+            else:
+                nums[a] = - abs(nums[a])
+            
+        for i in range(1, n):
+            if nums[i] > 0:
+                return i
+        
+        if nums[0] > 0:
+            return n
+            
+        return n + 1
