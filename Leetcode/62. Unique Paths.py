2f46b9c5a146765a30db5e1c5dda2ef232fc2ccd
Victor <career@victorplusc.com>
2019-09-06 22:18:00 -0400

    Create 62. Unique Paths.py

---
+++ Leetcode/62. Unique Paths.py
@@ -0,0 +1,41 @@
+"""
+62. Unique Paths
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+"""
+
+# Time complexity: O(N*M)
+# Space complexity: O(N*M)
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        grid = [[1 for i in range(n)] for i in range(m)]
+        
+        for y in range(1, m):
+            for x in range(1, n):
+                grid[y][x] = grid[y-1][x] + grid[y][x-1]
+                
+        return grid[-1][-1]
