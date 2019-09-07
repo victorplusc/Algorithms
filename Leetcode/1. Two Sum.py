6a3500768beeb9f95dfc437580a39e11a862137a
Victor <career@victorplusc.com>
2019-09-07 17:35:53 -0400

    Update 1. Two Sum.py

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -4,15 +4,18 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 """
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, num in enumerate(nums):
-            d[num] = i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        indices = {num:i for i, num in enumerate(nums)}
         
-        for j, num in enumerate(nums):
-            if target-num in d and j != d[target-num]:
-                return sorted([d[target-num], j])
+        for i, num in enumerate(nums):
+            if target - num in indices and i != indices[target-num]:
+                return sorted([indices[target-num], i])
                 
 # Time complexity: O(N)
 # Space complexity: O(N)
