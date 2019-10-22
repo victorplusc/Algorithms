042c375728ab73d5ac0d0273af8346987b0c477b
Victor <career@victorplusc.com>
2019-10-21 20:45:14 -0400

    Create 1221. Split a String in Balanced Strings.py

---
+++ Leetcode/1221. Split a String in Balanced Strings.py
@@ -0,0 +1,39 @@
+"""
+1221. Split a String in Balanced Strings
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+"""
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        n = 0
+        counter = 0
+        for c in s:
+            if c == "L":
+                counter += 1
+            else:
+                counter -= 1
+            
+            if counter == 0:
+                n += 1
+        return n
