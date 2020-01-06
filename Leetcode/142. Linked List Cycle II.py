f51f3ed6e509475ddd22ce91d5e3302fa41e5f6d
Victor <career@victorplusc.com>
2020-01-06 18:13:11 -0500

    Create 142. Linked List Cycle II.py

---
+++ Leetcode/142. Linked List Cycle II.py
@@ -0,0 +1,75 @@
+"""
+142. Linked List Cycle II
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+Note: Do not modify the linked list.
+
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+
+
+Follow-up:
+Can you solve it without using extra space?
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # Time complexity: O(N)
+    # Space complexity: O(1) 
+    def find_intersection(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return slow
+        return None
+    
+    
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        
+        intersection = self.find_intersection(head)
+        if not intersection:
+            return None
+
+        curr = head
+        while curr != intersection:
+            curr = curr.next
+            intersection = intersection.next
+        return curr
+    
+    
+    # Time complexity: O(N)
+    # Space complexity: O(N)
+    def detectCycle(self, head: ListNode) -> ListNode:
+        seen = set()
+        curr = head
+        while curr:
+            seen.add(curr)
+            curr = curr.next
+            if curr in seen:
+                return curr
+        return None
