166ecad14616e0b6d717c8baacf37954e45f06b8
Victor <career@victorplusc.com>
2020-01-11 12:34:56 -0500

    Create 422. Valid Word Square.py

---
+++ Leetcode/422. Valid Word Square.py
@@ -0,0 +1,81 @@
+"""
+422. Valid Word Square
+Given a sequence of words, check whether it forms a valid word square.
+
+A sequence of words forms a valid word square if the kth row and column read the exact same string, where 0 ≤ k < max(numRows, numColumns).
+
+Note:
+The number of words given is at least 1 and does not exceed 500.
+Word length will be at least 1 and does not exceed 500.
+Each word contains only lowercase English alphabet a-z.
+Example 1:
+
+Input:
+[
+  "abcd",
+  "bnrt",
+  "crmy",
+  "dtye"
+]
+
+Output:
+true
+
+Explanation:
+The first row and first column both read "abcd".
+The second row and second column both read "bnrt".
+The third row and third column both read "crmy".
+The fourth row and fourth column both read "dtye".
+
+Therefore, it is a valid word square.
+Example 2:
+
+Input:
+[
+  "abcd",
+  "bnrt",
+  "crm",
+  "dt"
+]
+
+Output:
+true
+
+Explanation:
+The first row and first column both read "abcd".
+The second row and second column both read "bnrt".
+The third row and third column both read "crm".
+The fourth row and fourth column both read "dt".
+
+Therefore, it is a valid word square.
+Example 3:
+
+Input:
+[
+  "ball",
+  "area",
+  "read",
+  "lady"
+]
+
+Output:
+false
+
+Explanation:
+The third row reads "read" while the third column reads "lead".
+
+Therefore, it is NOT a valid word square.
+"""
+# Time complexity: O(M*N)
+# Space complexity: O(1)
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        if not words or len(words) == 0:
+            return True
+        m = len(words)
+        
+        for y in range(len(words)):
+            for x in range(len(words[y])):
+                if x >= m or len(words[x]) <= y or words[y][x] != words[x][y]:
+                    return False
+        return True
