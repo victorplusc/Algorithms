81939353435c8407ab7b6179f3492cf883afa2c3
Victor <career@victorplusc.com>
2020-01-11 21:35:47 -0500

    Update 277. Find the Celebrity.py

--- Leetcode/277. Find the Celebrity.py
+++ Leetcode/277. Find the Celebrity.py
@@ -6,11 +6,8 @@ Now you want to find out who the celebrity is or verify that there is not one. T
 
 You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n). There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
 
- 
 
 Example 1:
-
-
 Input: graph = [
   [1,1,0],
   [0,1,0],
@@ -18,9 +15,8 @@ Input: graph = [
 ]
 Output: 1
 Explanation: There are three persons labeled with 0, 1 and 2. graph[i][j] = 1 means person i knows person j, otherwise graph[i][j] = 0 means person i does not know person j. The celebrity is the person labeled as 1 because both 0 and 2 know him but 1 does not know anybody.
+
 Example 2:
-
-
 Input: graph = [
   [1,0,1],
   [1,1,0],
@@ -31,7 +27,6 @@ Explanation: There is no celebrity.
  
 
 Note:
-
 The directed graph is represented as an adjacency matrix, which is an n x n matrix where a[i][j] = 1 means person i knows person j while a[i][j] = 0 means the contrary.
 Remember that you won't have direct access to the adjacency matrix.
 """
