de07cf6cde02fd38a5ef826adda6287c1e0fb402
Victor <career@victorplusc.com>
2020-02-05 22:11:26 -0500

    Update 91. Decode Ways.py

--- Leetcode/91. Decode Ways.py
+++ Leetcode/91. Decode Ways.py
@@ -19,10 +19,15 @@ Input: "226"
 Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 """
-# Time complexity: O(N)
-# Space complexity: O(N)
+
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # return self.dp(s)
+        return self.constant_space(s)
+    
+    # Time complexity: O(N)
+    # Space complexity: O(N)
+    def dp(self, s):
         if not s:
             return 0
         n = len(s)
@@ -37,4 +42,23 @@ class Solution:
             if 10 <= second <= 26:
                 dp[i] += dp[i-2]
         return dp[n]
-    
+
+    # Time complexity: O(N)
+    # Space complexity: O(1)
+    def constant_space(self, s):
+        if not s:
+            return 0
+        n = len(s)
+        second = 1
+        first = 1 if s[0] != "0" else 0
+        for i in range(2, n+1):
+            single = int(s[i-1:i])
+            double = int(s[i-2:i])
+            curr = 0
+            if single != 0:
+                curr += first
+            if 10 <= double <= 26:
+                curr += second
+            second = first
+            first = curr
+        return first
