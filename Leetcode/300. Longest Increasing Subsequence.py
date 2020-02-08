3988e5ca0b2545950106c5bfdb8301cf7e0fd363
Victor <career@victorplusc.com>
2020-02-07 21:30:18 -0500

    Update 300. Longest Increasing Subsequence.py

--- Leetcode/300. Longest Increasing Subsequence.py
+++ Leetcode/300. Longest Increasing Subsequence.py
@@ -16,7 +16,9 @@ Follow up: Could you improve it to O(n log n) time complexity?
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         # return self.naive_recursive(nums, -float('inf'), 0)
-        return self.memoized(nums, -float('inf'), 0, collections.defaultdict(dict))
+        # return self.memoized(nums, -float('inf'), 0, collections.defaultdict(dict))
+        # return self.dp(nums)
+        return self.dp_bin_search(nums)
         
     # Time complexity: O(2**N)
     # Space complexity: O(N**2)
@@ -42,3 +44,40 @@ class Solution:
         not_taken = self.memoized(nums, prev, curr+1, memo)
         memo[prev][curr] = max(taken, not_taken)
         return memo[prev][curr]
+    
+    
+    # Time complexity: O(N**2)
+    # Space complexity: O(N)
+    def dp(self, nums):
+        if not nums:
+            return 0
+        dp = [0] * len(nums)
+        dp[0] = 1
+        longest = 1
+        for i in range(1, len(nums)):
+            curr_max = 0
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    curr_max = max(curr_max, dp[j])
+            dp[i] = curr_max + 1
+            longest = max(curr_max, dp[i])
+        return longest
+    
+    
+    # Time complexity: O(N log N)
+    # Space complexity: O(N)
+    def dp_bin_search(self, nums):
+        dp = [0] * len(nums)
+        longest = 0
+        for n in nums:
+            left, right = 0, longest
+            while left != right:
+                mid = left + (right - left) // 2
+                if dp[mid] < n:
+                    left = mid + 1
+                else:
+                    right = mid
+            dp[left] = n
+            longest = max(longest, left + 1)
+        return longest
+    
