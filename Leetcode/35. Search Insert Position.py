97fe10495671d8c9cc0764f38704031d0e1cf0b3
Victor <career@victorplusc.com>
2020-02-09 22:02:34 -0500

    Create 35. Search Insert Position.py

---
+++ Leetcode/35. Search Insert Position.py
@@ -0,0 +1,39 @@
+"""
+35. Search Insert Position
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+"""
+# Time complexity: O(lgN)
+# Space complexity: O(1)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid-1
+        return left
