fdd7a6550c73562ffcc7aa8d45e1a4959933684b
Victor <career@victorplusc.com>
2020-03-16 21:48:23 -0400

    Create 848. Shifting Letters.py

---
+++ Leetcode/848. Shifting Letters.py
@@ -0,0 +1,37 @@
+"""
+848. Shifting Letters
+We have a string S of lowercase letters, and an integer array shifts.
+
+Call the shift of a letter, the next letter in the alphabet, (wrapping around so that 'z' becomes 'a'). 
+
+For example, shift('a') = 'b', shift('t') = 'u', and shift('z') = 'a'.
+
+Now for each shifts[i] = x, we want to shift the first i+1 letters of S, x times.
+
+Return the final string after all such shifts to S are applied.
+
+Example 1:
+Input: S = "abc", shifts = [3,5,9]
+Output: "rpl"
+Explanation: 
+We start with "abc".
+After shifting the first 1 letters of S by 3, we have "dbc".
+After shifting the first 2 letters of S by 5, we have "igc".
+After shifting the first 3 letters of S by 9, we have "rpl", the answer.
+
+Note:
+1 <= S.length = shifts.length <= 20000
+0 <= shifts[i] <= 10 ^ 9
+"""
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def shiftingLetters(self, S: str, shifts: List[int]) -> str:
+        shifted_sum = sum(shifts)%26
+        output = []
+        
+        for i, c in enumerate(S):
+            shift = (ord(c)-ord("a")+shifted_sum)%26
+            output.append(chr(ord("a") + shift))
+            shifted_sum = (shifted_sum - shifts[i])%26
+        return "".join(output)
