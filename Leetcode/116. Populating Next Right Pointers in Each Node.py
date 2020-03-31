8a1fbed2a5c9e5c0ce5d6fcacad8de193438e0ef
Victor <career@victorplusc.com>
2020-03-30 23:30:12 -0400

    Update 116. Populating Next Right Pointers in Each Node.py

--- Leetcode/116. Populating Next Right Pointers in Each Node.py
+++ Leetcode/116. Populating Next Right Pointers in Each Node.py
@@ -45,14 +45,14 @@ class Solution:
             return root
         
         leftmost = root
-        
         while leftmost.left:
             head = leftmost
             while head:
                 head.left.next = head.right
-                
+
                 if head.next:
                     head.right.next = head.next.left
+
                 head = head.next
             leftmost = leftmost.left
         return root
