c8f26a66280954940be0831f658b0151ba6f5c12
Victor <career@victorplusc.com>
2020-04-01 21:59:24 -0400

    Create 136. Single Number.py

---
+++ Leetcode/136. Single Number.py
@@ -0,0 +1,23 @@
+"""
+136. Single Number
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+Input: [4,1,2,1,2]
+Output: 4
+"""
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        i = 0
+        for n in nums:
+            i ^= n
+        return i
