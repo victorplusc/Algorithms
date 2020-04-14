dd71d233bba2617b0f8a7ca29ce2c03aac81b4d3
Victor <career@victorplusc.com>
2020-04-13 23:09:49 -0400

    Create 13. Contiguous Array.py

---
+++ Leetcode/30-Day LeetCoding Challenge/13. Contiguous Array.py
@@ -0,0 +1,29 @@
+"""
+Contiguous Array
+Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+Note: The length of the given binary array will not exceed 50,000.
+"""
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        mapping = {0: -1}
+        longest = count = 0
+        for i in range(len(nums)):
+            count += 1 if nums[i] == 1 else -1
+            if count in mapping:
+                longest = max(longest, i-mapping[count])
+            else:
+                mapping[count] = i
+        return longest
