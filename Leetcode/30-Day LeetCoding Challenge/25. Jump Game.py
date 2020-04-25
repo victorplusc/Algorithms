820778334ea9f73b47ec798c3c4f8e095b9005a0
Victor <career@victorplusc.com>
2020-04-25 10:06:22 -0400

    Create 25. Jump Game.py

---
+++ Leetcode/30-Day LeetCoding Challenge/25. Jump Game.py
@@ -0,0 +1,32 @@
+"""
+Jump Game
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.
+"""
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        last_pos = len(nums)-1
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] >= last_pos - i:
+                last_pos = i
+        return last_pos == 0
