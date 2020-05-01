d22b78f92b3c75185ffb7a61f736d3fa5f5de97d
Victor <career@victorplusc.com>
2020-05-01 01:34:49 -0400

    Update 30. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.py

--- Leetcode/30-Day LeetCoding Challenge/30. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.py
+++ Leetcode/30-Day LeetCoding Challenge/30. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.py
@@ -1,1 +1,52 @@
+"""
+Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
 
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+
+Example 1:
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+
+Example 2:
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+
+Example 3:
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+Output: false
+Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+ 
+
+Constraints:
+1 <= arr.length <= 5000
+0 <= arr[i] <= 9
+Each node's value is between [0 - 9].
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def isValidSequence(self, root: TreeNode, arr: List[int]) -> bool:
+        def dfs(node, idx):
+            if node and node.val != arr[idx]:
+                return False
+            if idx == len(arr)-1:
+                return not node.left and not node.right
+
+            left = node.left and dfs(node.left, idx+1)
+            right = node.right and dfs(node.right, idx+1)
+            return left or right
+            
+        return dfs(root, 0)
