465077e083327b9be130d32d274ae3965d1e7966
Victor <career@victorplusc.com>
2020-05-02 17:18:09 -0400

    Create 2. Jewels and Stones.py

---
+++ Leetcode/May LeetCoding Challenge/2. Jewels and Stones.py
@@ -0,0 +1,28 @@
+"""
+Jewels and Stones
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+Input: J = "z", S = "ZZ"
+Output: 0
+
+Note:
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+"""
+# Time complexity: O(N+M)
+# Space complexity: O(N+M)
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        n = 0
+        jewels = set(J)
+        for c in S:
+            if c in jewels:
+                n += 1
+        return n
