c55fc5ed71cf9973929ca25a23c47a8e89c7b9ec
Victor <career@victorplusc.com>
2020-05-07 22:59:18 -0400

    Create 7. Cousins in Binary Tree.py

---
+++ Leetcode/May LeetCoding Challenge/7. Cousins in Binary Tree.py
@@ -0,0 +1,56 @@
+"""
+Cousins in Binary Tree
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+Return true if and only if the nodes corresponding to the values x and y are cousins.
+
+Example 1:
+Input: root = [1,2,3,4], x = 4, y = 3
+Output: false
+
+Example 2:
+Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+Output: true
+
+Example 3:
+Input: root = [1,2,3,null,4], x = 2, y = 3
+Output: false
+ 
+Note:
+The number of nodes in the tree will be between 2 and 100.
+Each node has a unique integer value from 1 to 100.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        x_node = None
+        y_node = None
+        queue = collections.deque([[root, None]]) # [root, parent]
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node, parent = queue.popleft()
+                if not node: continue
+                if node.val == x:
+                    x_node = parent
+                if node.val == y:
+                    y_node = parent
+                queue.append([node.left, node])
+                queue.append([node.right, node])
+            if (x_node or y_node) and not (x_node and y_node):
+                return False
+            elif x_node and y_node and x_node != y_node:
+                return True
+
+        return False
