ee3679da2300065440efad9fef37f62182f6be5a
Victor <career@victorplusc.com>
2020-05-18 01:08:12 -0400

    Update 17. Find All Anagrams in a String.py

--- Leetcode/May LeetCoding Challenge/17. Find All Anagrams in a String.py
+++ Leetcode/May LeetCoding Challenge/17. Find All Anagrams in a String.py
@@ -30,7 +30,23 @@ The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 """
 # Time complexity: O(N)
-# Space complexity: O(K)
+# Space complexity: O(1), because there can only be 26 different characters
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        x = ord('a')
         
+        size = len(p)
+        anagram = [0 for _ in range(26)]
+        for c in p: anagram[x-ord(c)] += 1
+        count = [0 for _ in range(26)]
+
+        output = []
+        for i, c in enumerate(s):
+            count[x-ord(c)] += 1
+            if i >= size:
+                count[x-ord(s[i-size])] -= 1
+
+            if count == anagram:
+                output.append(i-size+1)
+                
+        return output
