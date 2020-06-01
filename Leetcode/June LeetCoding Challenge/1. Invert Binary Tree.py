33b825c8be998d4f47ec301d821a81c0dfa7d407
Victor <career@victorplusc.com>
2020-06-01 13:32:15 -0400

    Create 1. Invert Binary Tree.py

---
+++ Leetcode/June LeetCoding Challenge/1. Invert Binary Tree.py
@@ -0,0 +1,38 @@
+"""
+Invert Binary Tree
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time complexity: O(N)
+# Space complexity: O(H)
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
