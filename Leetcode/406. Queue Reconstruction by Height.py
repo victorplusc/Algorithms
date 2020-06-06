d0319d0cb7c16f531e8f54a1a4ceab2c49ed4b25
Victor <career@victorplusc.com>
2020-06-06 19:04:18 -0400

    Create 406. Queue Reconstruction by Height.py

---
+++ Leetcode/406. Queue Reconstruction by Height.py
@@ -0,0 +1,26 @@
+"""
+406. Queue Reconstruction by Height
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+"""
+# Time complexity: O(N**2)
+# Space complexity: O(N)
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))
+        output = []
+        
+        for (h, k) in people:
+            output.insert(k, (h, k))
+        
+        return output
