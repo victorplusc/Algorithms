526af503924f022aa7a12aa61e2b615d1254b8a6
Victor <career@victorplusc.com>
2020-06-22 12:14:45 -0400

    Create 1231. Divide Chocolate.py

---
+++ Leetcode/1231. Divide Chocolate.py
@@ -0,0 +1,52 @@
+"""
+1231. Divide Chocolate
+You have one chocolate bar that consists of some chunks. Each chunk has its own sweetness given by the array sweetness.
+
+You want to share the chocolate with your K friends so you start cutting the chocolate bar into K+1 pieces using K cuts, each piece consists of some consecutive chunks.
+
+Being generous, you will eat the piece with the minimum total sweetness and give the other pieces to your friends.
+
+Find the maximum total sweetness of the piece you can get by cutting the chocolate bar optimally.
+
+Example 1:
+Input: sweetness = [1,2,3,4,5,6,7,8,9], K = 5
+Output: 6
+Explanation: You can divide the chocolate to [1,2,3], [4,5], [6], [7], [8], [9]
+
+Example 2:
+Input: sweetness = [5,6,7,8,9,1,2,3,4], K = 8
+Output: 1
+Explanation: There is only one way to cut the bar into 9 pieces.
+
+Example 3:
+Input: sweetness = [1,2,2,1,2,2,1,2,2], K = 2
+Output: 5
+Explanation: You can divide the chocolate to [1,2,2], [1,2,2], [1,2,2]
+
+Constraints:
+0 <= K < sweetness.length <= 10^4
+1 <= sweetness[i] <= 10^5
+"""
+# Time complexity: O(N log N)
+# Space complexity: O(1)
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
+        def pieces(mid):
+            total = count = 0
+            for piece in sweetness:
+                total += piece
+                if total >= mid:
+                    count += 1
+                    total = 0
+            return count
+        
+        left = 1
+        right = sum(sweetness) // (K+1)
+        
+        while left < right:
+            mid = (left+right+1)//2
+            if pieces(mid) < K+1:
+                right = mid - 1
+            else:
+                left = mid
+        return right
