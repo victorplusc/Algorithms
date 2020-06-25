54544e9508b4d5d10f8a150282f907a201f531d2
Victor <career@victorplusc.com>
2020-06-25 10:36:29 -0400

    Update 22. Generate Parentheses.py

--- Leetcode/22. Generate Parentheses.py
+++ Leetcode/22. Generate Parentheses.py
@@ -12,19 +12,23 @@ For example, given n = 3, a solution set is:
   "()()()"
 ]
 """
-
-# Time and space complexity: just say O(2**n)
+# Time complexity: just say exponential
+# Space complexity: just say exponential
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.perms = []
-        self.backtrack(n, [], 0, 0)
-        return self.perms
-    
-    def backtrack(self, n, curr, left, right):
-        if len(curr) == 2*n:
-            self.perms.append("".join(curr))
-            return
-        if left < n:
-            self.backtrack(n, [_ for _ in curr] + ["("], left + 1, right)
-        if right < left:
-            self.backtrack(n, [_ for _ in curr] + [")"], left, right + 1)
+        perms = []
+        def backtrack(curr = [], left=0, right=0):
+            if len(curr) == 2*n:
+                perms.append("".join(curr))
+                return
+            if left < n:
+                curr.append("(")
+                backtrack(curr, left+1, right)
+                curr.pop()
+            if right < left:
+                curr.append(")")
+                backtrack(curr, left, right+1)
+                curr.pop()
+ 
+        backtrack()
+        return perms
