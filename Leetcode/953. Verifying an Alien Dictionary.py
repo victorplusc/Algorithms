dc9af79a989eb602d246a233fe7973e3623034a6
Victor <career@victorplusc.com>
2020-07-05 11:52:43 -0400

    Update 953. Verifying an Alien Dictionary.py

--- Leetcode/953. Verifying an Alien Dictionary.py
+++ Leetcode/953. Verifying an Alien Dictionary.py
@@ -5,23 +5,21 @@ In an alien language, surprisingly they also use english lowercase letters, but 
 Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
 
 Example 1:
-
 Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
 Output: true
 Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
-Example 2:
 
+Example 2:
 Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
 Output: false
 Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
-Example 3:
 
+Example 3:
 Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
 Output: false
 Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
 
 Note:
-
 1 <= words.length <= 100
 1 <= words[i].length <= 20
 order.length == 26
@@ -37,16 +35,13 @@ class Solution:
             word1 = words[i]
             word2 = words[i+1]
             diff = False
-            
-            for k in range(min(len(word1), len(word2))):
-                if word1[k] != word2[k]:
-                    if dictionary[word1[k]] > dictionary[word2[k]]:
+            for j in range(min(len(word1), len(word2))):
+                if word1[j] != word2[j]:
+                    if dictionary[word1[j]] > dictionary[word2[j]]:
                         return False
                     diff = True
                     break
+
+            if not diff and len(word1) > len(word2): return False
                     
-            if not diff:
-                if len(word1) > len(word2):
-                    return False
-                
         return True
