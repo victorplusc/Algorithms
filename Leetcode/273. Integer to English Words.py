9a0438e5596fcaa6b6a21a07ad4dcf9fb93d8880
Victor <career@victorplusc.com>
2020-07-07 10:24:56 -0400

    Update 273. Integer to English Words.py

--- Leetcode/273. Integer to English Words.py
+++ Leetcode/273. Integer to English Words.py
@@ -25,52 +25,52 @@ class Solution:
     def numberToWords(self, num: int) -> str:
         if not num:
             return "Zero"
+        
         billions = num // 1000000000
-        millions = (num-billions*1000000000) // 1000000
-        thousands = (num - billions*1000000000 - millions*1000000) // 1000
-        rem = (num - billions*1000000000 - millions*1000000 - thousands*1000)
+        millions = (num%1000000000)//1000000
+        thousands = (num%1000000000%1000000) // 1000
+        remainder = (num%1000000000%1000000%1000)
         
         output = []
         if billions:
-            output.append(self.three(billions) + " Billion")
-        
+            output.append(self.process_hundreds(billions) + " Billion")
+            
         if millions:
             if output: output.append(" ")
-            output.append(self.three(millions) + " Million")
+            output.append(self.process_hundreds(millions) + " Million")
         
         if thousands:
             if output: output.append(" ")
-            output.append(self.three(thousands) + " Thousand")
+            output.append(self.process_hundreds(thousands) + " Thousand")
             
-        if rem:
+        if remainder:
             if output: output.append(" ")
-            output.append(self.three(rem))
+            output.append(self.process_hundreds(remainder))
         
         return "".join(output)
     
-    def three(self, num):
+    def process_hundreds(self, num):
         hundreds = num//100
-        rem = num-hundreds*100
-        if hundreds and rem:
-            return self.one(hundreds) + " Hundred " + self.two(rem)
-        elif not hundreds and rem:
-            return self.two(rem)
-        elif hundreds and not rem:
-            return self.one(hundreds) + " Hundred"
-    
-    def two(self, num):
-        if not num:
-            return ""
+        remainder = num%100
+        if hundreds and remainder:
+            return self.process_single(hundreds) + " Hundred " + self.process_double(remainder)
+        if hundreds and not remainder:
+            return self.process_single(hundreds) + " Hundred"
+        if not hundreds and remainder:
+            return self.process_double(remainder)
+        
+    def process_double(self, num):
+        if not num: return ""
         elif num < 10:
-            return self.one(num)
+            return self.process_single(num)
         elif num < 20:
-            return self.sub_20(num)
+            return self.process_sub_20(num)
         else:
-            tens = num // 10
-            rem = num - tens*10
-            return self.ten(tens) + " " + self.one(rem) if rem else self.ten(tens)
+            tens = num//10
+            remainder = num%10
+            return self.process_tens(tens) + " " + self.process_single(remainder) if remainder else self.process_tens(tens)
     
-    def ten(self, num):
+    def process_tens(self, num):
         mapping = {
             2: "Twenty",
             3: "Thirty",
@@ -82,8 +82,22 @@ class Solution:
             9: "Ninety"
         }
         return mapping[num]
+
+    def process_single(self, num):
+        mapping = {
+            1: "One",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine"
+        }
+        return mapping[num]
     
-    def sub_20(self, num):
+    def process_sub_20(self, num):
         mapping = {
             10: "Ten",
             11: "Eleven",
@@ -95,19 +109,5 @@ class Solution:
             17: "Seventeen",
             18: "Eighteen",
             19: "Nineteen"
-        }
-        return mapping[num]
-    
-    def one(self, num):
-        mapping = {
-            1: "One",
-            2: "Two",
-            3: "Three",
-            4: "Four",
-            5: "Five",
-            6: "Six",
-            7: "Seven",
-            8: "Eight",
-            9: "Nine"
         }
         return mapping[num]
