a325d6632f2cae64ae0d08364981e60861c2bff2
Victor <career@victorplusc.com>
2020-07-08 23:32:56 -0400

    Update 15. 3Sum.py

--- Leetcode/15. 3Sum.py
+++ Leetcode/15. 3Sum.py
@@ -3,11 +3,9 @@
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Note:
-
 The solution set must not contain duplicate triplets.
 
 Example:
-
 Given array nums = [-1, 0, 1, 2, -1, -4],
 
 A solution set is:
@@ -16,34 +14,59 @@ A solution set is:
   [-1, -1, 2]
 ]
 """
-
-# Time complexity: O(N**2)
-# Space complexity: O(N)
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        indices = []
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        return self.two_pointers(nums)
+        return self.hash_set(nums)
+    
+    # Time complexity: O(N**2)
+    # Space complexity: O(N**2)
+    def hash_set(self, nums):
+        n = len(nums)
+        seen = {}
+        found, dupes = set(), set()
+        
+        sums = []
+        for i in range(n):
+            if nums[i] not in dupes: 
+                dupes.add(nums[i])
+                
+                for j in range(i+1, n):
+                    complement = -nums[i] - nums[j]
+                    if complement in seen and seen[complement] == i:
+                        min_val = min(nums[i], nums[j], complement)
+                        max_val = max(nums[i], nums[j], complement)
+                        if (min_val, max_val) not in found:
+                            found.add((min_val, max_val))
+                            sums.append([nums[i], nums[j], complement])
+                    seen[nums[j]] = i
+        return sums
+    
+    # Time complexity: O(N**2)
+    # Space complexity: O(N)
+    def two_pointers(self, nums):
+        n = len(nums)
         nums.sort()
         
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            left, right = i+1, len(nums)-1
-            while left < right:
-                s = nums[i] + nums[left] + nums[right]
-                if s < 0:
-                    left += 1
-                elif s > 0:
-                    right -= 1
-                else:
-                    indices.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-        return indices
+        sums = []
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i-1] != nums[i]:
+                self.two_sum(nums, i, sums)
+        return sums
+    
+    def two_sum(self, nums, i, sums):
+        left, right = i+1, len(nums)-1
+        
+        while left < right:
+            curr = nums[i] + nums[left] + nums[right]
+            
+            if curr < 0 or (left > i+1 and nums[left] == nums[left-1]):
+                left += 1
+            elif curr > 0 or (right < len(nums)-1 and nums[right] == nums[right+1]):
+                right -= 1
+            else:
+                sums.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -= 1
