51ba883394258175e0db803df84afefde5cb2993
Victor <career@victorplusc.com>
2020-07-09 20:00:36 -0400

    Create 582. Kill Process.py

---
+++ Leetcode/582. Kill Process.py
@@ -0,0 +1,47 @@
+"""
+582. Kill Process
+Given n processes, each process has a unique PID (process id) and its PPID (parent process id).
+
+Each process only has one parent process, but may have one or more children processes. This is just like a tree structure. Only one process has PPID that is 0, which means this process has no parent process. All the PIDs will be distinct positive integers.
+
+We use two list of integers to represent a list of processes, where the first list contains PID for each process and the second list contains the corresponding PPID.
+
+Now given the two lists, and a PID representing a process you want to kill, return a list of PIDs of processes that will be killed in the end. You should assume that when a process is killed, all its children processes will be killed. No order is required for the final answer.
+
+Example 1:
+Input: 
+pid =  [1, 3, 10, 5]
+ppid = [3, 0, 5, 3]
+kill = 5
+Output: [5,10]
+Explanation: 
+           3
+         /   \
+        1     5
+             /
+            10
+Kill 5 will also kill 10.
+
+Note:
+The given kill id is guaranteed to be one of the given PIDs.
+n >= 1.
+"""
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:         
+        graph = collections.defaultdict(list)
+        
+        for parent, process in zip(ppid, pid):
+            graph[parent].append(process)
+        
+        killed = []
+        
+        q = collections.deque([kill])
+        while q:
+            front = q.popleft()
+            killed.append(front)
+            for child in graph[front]:
+                q.append(child)
+        
+        return killed
