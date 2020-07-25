30285b6ea63eccb9dae85ac875a4348754e29fb1
Victor <career@victorplusc.com>
2020-07-25 14:12:23 -0400

    Create Simple Array Sum.py

---
+++ Hackerrank/Simple Array Sum.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+from __future__ import print_function
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(raw_input())
+
+    ar = map(int, raw_input().rstrip().split())
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
