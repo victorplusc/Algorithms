e9ce254dde1fe7346959c9c44f0836592f4f7154
Victor <career@victorplusc.com>
2020-07-28 11:17:23 -0400

    Create 621. Task Scheduler.py

---
+++ Leetcode/621. Task Scheduler.py
@@ -0,0 +1,54 @@
+"""
+621. Task Scheduler
+You are given a char array representing tasks CPU need to do. It contains capital letters A to Z where each letter represents a different task. Tasks could be done without the original order of the array. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+
+You need to return the least number of units of times that the CPU will take to finish all the given tasks.
+
+Example 1:
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+Output: 8
+Explanation: 
+A -> B -> idle -> A -> B -> idle -> A -> B
+There is at least 2 units of time between any two same tasks.
+
+Example 2:
+Input: tasks = ["A","A","A","B","B","B"], n = 0
+Output: 6
+Explanation: On this case any permutation of size 6 would work since n = 0.
+["A","A","A","B","B","B"]
+["A","B","A","B","A","B"]
+["B","B","B","A","A","A"]
+...
+And so on.
+
+Example 3:
+Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+Output: 16
+Explanation: 
+One possible solution is
+A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+
+Constraints:
+The number of tasks is in the range [1, 10000].
+The integer n is in the range [0, 100].
+"""
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        frequencies = [0] * 26
+        for t in tasks:
+            frequencies[ord(t)-ord("A")] += 1
+        
+        frequencies.sort()
+        
+        f_max = frequencies.pop()
+        idle_time = (f_max - 1) * n
+        
+        while frequencies and idle_time > 0:
+            idle_time -= min(f_max-1, frequencies.pop())
+        
+        idle_time = max(0, idle_time)
+        return idle_time + len(tasks)
