1bd3d7e44a567dd6315bf150318c19cef6bf9f12
Victor <career@victorplusc.com>
2020-08-08 18:18:37 -0400

    Create Delete a Node.py

---
+++ Hackerrank/Delete a Node.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    if not head:
+        return None
+    if position == 0:
+        return head.next
+    idx = 0
+    curr = head
+    prev = None
+    while curr:
+        
+        prev = curr
+        curr = curr.next
+        idx += 1
+        if idx == position:
+            prev.next = curr.next
+    return head
+
+if __name__ == '__main__':
