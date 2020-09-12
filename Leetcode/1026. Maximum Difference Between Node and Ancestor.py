82fe9ab5cdf65232d16d39fb547a06cb30f58b2f
Victor <career@victorplusc.com>
2020-09-12 14:17:08 -0400

    Update 1026. Maximum Difference Between Node and Ancestor.py

--- Leetcode/1026. Maximum Difference Between Node and Ancestor.py
+++ Leetcode/1026. Maximum Difference Between Node and Ancestor.py
@@ -5,7 +5,6 @@ Given the root of a binary tree, find the maximum value V for which there exists
 (A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.)
 
 Example 1:
-
 Input: [8,3,10,1,6,null,14,null,null,4,7,13]
 Output: 7
 Explanation: 
@@ -15,27 +14,27 @@ We have various ancestor-node differences, some of which are given below :
 |8 - 1| = 7
 |10 - 13| = 3
 Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
-"""
 
+Note:
+The number of nodes in the tree is between 2 and 5000.
+Each node will have value between 0 and 100000.
+"""
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 # Time complexity: O(N)
-# Space complexity: O(N)
+# Space complexity: O(H)
 class Solution:
     def maxAncestorDiff(self, root: TreeNode) -> int:
-        if root:
-            return self.ancestor_helper(root, root.val, root.val)
-        
-    def ancestor_helper(self, root, max_ancestor, min_ancestor):
-        if root:
-            max_ancestor = max(root.val, max_ancestor)
-            min_ancestor = min(root.val, min_ancestor)
-            left = self.ancestor_helper(root.left, max_ancestor, min_ancestor) if root.left else -1
-            right = self.ancestor_helper(root.right, max_ancestor, min_ancestor) if root.right else -1
-            
-            return max(max_ancestor-min_ancestor, left, right)
+        def dfs(node, max_val, min_val):
+            if not node:
+                return max_val - min_val
+            max_val = max(max_val, node.val)
+            min_val = min(min_val, node.val)
+            left = dfs(node.left, max_val, min_val)
+            right = dfs(node.right, max_val, min_val)
+            return max(left, right)
+        return dfs(root, root.val, root.val)
