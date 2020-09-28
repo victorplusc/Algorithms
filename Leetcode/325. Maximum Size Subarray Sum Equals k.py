e7c00a467730c385d3a484a8a6b01f4d6e6c5c51
Victor <career@victorplusc.com>
2020-09-28 14:37:46 -0400

    Create 325. Maximum Size Subarray Sum Equals k.py

---
+++ Leetcode/325. Maximum Size Subarray Sum Equals k.py
@@ -0,0 +1,33 @@
+"""
+325. Maximum Size Subarray Sum Equals k
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+
+Note:
+The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+
+Example 1:
+Input: nums = [1, -1, 5, -2, 3], k = 3
+Output: 4 
+Explanation: The subarray [1, -1, 5, -2] sums to 3 and is the longest.
+
+Example 2:
+Input: nums = [-2, -1, 2, 1], k = 1
+Output: 2 
+Explanation: The subarray [-1, 2] sums to 1 and is the longest.
+"""
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        curr_sum = 0
+        best = 0
+        mapping = {}
+        for i, val in enumerate(nums):
+            curr_sum += nums[i]
+            if curr_sum == k:
+                best = i+1
+            elif curr_sum-k in mapping:
+                best = max(best, i-mapping[curr_sum-k])
+            if curr_sum not in mapping:
+                mapping[curr_sum] = i
+        return best
