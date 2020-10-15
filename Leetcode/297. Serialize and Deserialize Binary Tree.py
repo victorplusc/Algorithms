cadad1409ebcb4d8d42fd99560a85a7ff9c73170
Victor <career@victorplusc.com>
2020-10-15 19:09:16 -0400

    Update 297. Serialize and Deserialize Binary Tree.py

--- Leetcode/297. Serialize and Deserialize Binary Tree.py
+++ Leetcode/297. Serialize and Deserialize Binary Tree.py
@@ -4,7 +4,6 @@ Serialization is the process of converting a data structure or object into a seq
 
 Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
 """
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -12,45 +11,52 @@ Design an algorithm to serialize and deserialize a binary tree. There is no rest
 #         self.left = None
 #         self.right = None
 
-# Time complexity: O(N)
-# Space complexity: O(N)
 class Codec:
 
+    # Time complexity: O(N)
+    # Space complexity: O(N)
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        return ",".join(self.serialize_helper(root, []))
-        
-    def serialize_helper(self, root, serialized):
         if not root:
-            serialized.append("X")
-        else:
-            serialized.append(str(root.val))
-            self.serialize_helper(root.left, serialized)
-            self.serialize_helper(root.right, serialized)
-        return serialized
+            return ""
+        serialized = []
+        def preorder(node):
+            if node:
+                serialized.append(str(node.val))
+                preorder(node.left)
+                preorder(node.right)
+            else:
+                serialized.append("#")
+        preorder(root)
+        return " ".join(serialized) #use space as our delimiter
         
-
+    # Time complexity: O(N)
+    # Space complexity: O(N)
     def deserialize(self, data):
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
-        root = self.deserialize_helper(data.split(","))
-        return root
-    
-    def deserialize_helper(self, data):
-        if data[0] == "X":
-            data.pop(0)
+        if not data:
             return None
-        root = TreeNode(int(data.pop(0)))
-        root.left = self.deserialize_helper(data)
-        root.right = self.deserialize_helper(data)
-        return root
+        
+        q = collections.deque(data.split(" "))
+        def helper():
+            val = q.popleft()
+            if val == "#":
+                return None
+            root = TreeNode(int(val))
+            root.left = helper()
+            root.right = helper()
+            return root
+        return helper()
+            
+        
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
