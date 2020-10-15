b1d676aa191925912edf8810cb4dc2c0cc1ef468
Victor <career@victorplusc.com>
2020-10-14 23:33:53 -0400

    Create 170. Two Sum III - Data structure design.py

---
+++ Leetcode/170. Two Sum III - Data structure design.py
@@ -0,0 +1,66 @@
+"""
+170. Two Sum III - Data structure design
+Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
+
+Implement the TwoSum class:
+
+TwoSum() Initializes the TwoSum object, with an empty array initially.
+void add(int number) Adds number to the data structure.
+boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false.
+
+Example 1:
+Input
+["TwoSum", "add", "add", "add", "find", "find"]
+[[], [1], [3], [5], [4], [7]]
+Output
+[null, null, null, null, true, false]
+
+Explanation
+TwoSum twoSum = new TwoSum();
+twoSum.add(1);   // [] --> [1]
+twoSum.add(3);   // [1] --> [1,3]
+twoSum.add(5);   // [1,3] --> [1,3,5]
+twoSum.find(4);  // 1 + 3 = 4, return true
+twoSum.find(7);  // No two integers sum up to 7, return false
+
+Constraints:
+-105 <= number <= 105
+-231 <= value <= 231 - 1
+At most 5 * 104 calls will be made to add and find.
+"""
+# Time complexity: O(N)
+# Space complexity: O(N)
+class TwoSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = collections.Counter()
+        
+
+    def add(self, number: int) -> None:
+        """
+        Add the number to an internal data structure..
+        """
+        self.nums[number] += 1
+        
+
+    def find(self, target: int) -> bool:
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+        for val in self.nums:
+            if val != target-val and target-val in self.nums:
+                return True
+            elif val == target-val:
+                if self.nums[val] > 1:
+                    return True
+        return False
+        
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
