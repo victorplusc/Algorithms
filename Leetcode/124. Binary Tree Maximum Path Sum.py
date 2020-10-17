c25bd14e2fd51c434c86c0861ff0e16f7ae927fc
Victor <career@victorplusc.com>
2020-10-17 00:50:22 -0400

    Update 124. Binary Tree Maximum Path Sum.py

--- Leetcode/124. Binary Tree Maximum Path Sum.py
+++ Leetcode/124. Binary Tree Maximum Path Sum.py
@@ -32,19 +32,34 @@ Output: 42
 #         self.right = right
 # Time complexity: O(N)
 # Space complexity: O(log N)
+"""
+Clarify examples, and path definition. E.g. 
+Input: [0,1,2]
+       0
+      / \
+     1   2
+Output: 3
+
+Algorithm:
+1) Base case:
+    for nodes without children, maximum gain is 0 from both branches
+2) For every node, we want to only take the left/right branches if we get a net gain. So if they are < 0, just don't explore it (represented by 0)
+3) However when we recurse upwards, we cannot just take the maximum of the whole tree, we must at most take one branch
+4) So we take the max(node.val+left, node.val+right)
+"""
 class Solution:
     def maxPathSum(self, root: TreeNode) -> int:
-        max_path = -float('inf')
+        max_path = float('-inf')
         def dfs(node):
             nonlocal max_path
             if not node:
                 return 0
-            
             left = max(dfs(node.left), 0)
             right = max(dfs(node.right), 0)
-            curr = node.val + left + right
-            max_path = max(curr, max_path)
+            curr_max = node.val + left + right
+            max_path = max(max_path, curr_max)
+            
             return max(node.val+left, node.val+right)
-
+            
         dfs(root)
         return max_path
