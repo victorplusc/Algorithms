d0a68341a30f4704c9bbdf13f721cdacb8579dec
Victor <career@victorplusc.com>
2021-07-05 10:58:29 -0400

    Update 566. Reshape the Matrix.py

--- Leetcode/566. Reshape the Matrix.py
+++ Leetcode/566. Reshape the Matrix.py
@@ -1,60 +1,46 @@
 """
 566. Reshape the Matrix
-In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
 
-You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+You are given an m x n matrix mat and two integers r and c representing the row number and column number of the wanted reshaped matrix.
 
-The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
 
-If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
 
 Example 1:
-Input: 
-nums = 
-[[1,2],
- [3,4]]
-r = 1, c = 4
-Output: 
-[[1,2,3,4]]
-Explanation:
-The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
-Example 2:
-Input: 
-nums = 
-[[1,2],
- [3,4]]
-r = 2, c = 4
-Output: 
-[[1,2],
- [3,4]]
-Explanation:
-There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
-Note:
-The height and width of the given matrix is in range [1, 100].
-The given r and c are all positive.
-"""
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
 
+Example 2:
+Input: mat = [[1,2],[3,4]], r = 2, c = 4
+Output: [[1,2],[3,4]]
+
+Constraints:
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+-1000 <= mat[i][j] <= 1000
+1 <= r, c <= 300
+"""
 # Time complexity: O(N*M)
 # Space complexity: O(N*M)
 class Solution:
-    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
-        self.using_math(nums, r, c)
-        if not nums or r*c != len(nums[0])*len(nums):
-            return nums
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if not mat or r*c != len(mat)*len(mat[0]):
+            return mat
+        if len(mat) == r and len(mat[0]) == c:
+            return mat
 
-        reshaped = []
-        temp = []
-        curr_x = 0
-        
-        for row in nums:
-            for v in row:
-                temp.append(v)
-                curr_x += 1
-                if curr_x == c:
-                    curr_x = 0
-                    reshaped.append(temp)
-                    temp = []
-        return reshaped
+        output = []
+        row_to_add = []
+        for y, row in enumerate(mat):
+            for x, val in enumerate(row):
+                row_to_add.append(val)
+                if len(row_to_add) == c:
+                    output.append(row_to_add)
+                    row_to_add = []
+        return output
     
     # Time complexity: O(N*M)
     # Space complexity: O(N*M), including result
