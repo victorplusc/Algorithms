9f793fa3fda8ccfde68185a8e473e411b7e59895
Victor <career@victorplusc.com>
2021-09-17 11:53:22 -0400

    Update 103. Binary Tree Zigzag Level Order Traversal.py

--- Leetcode/103. Binary Tree Zigzag Level Order Traversal.py
+++ Leetcode/103. Binary Tree Zigzag Level Order Traversal.py
@@ -1,50 +1,52 @@
 """
-103. Binary Tree Zigzag Level Order Traversal
 Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
 """
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-import collections
-
+# Time complexity: O(N)
+# Space complexity: O(N)
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        zigzag = []
-        deque = collections.deque()
-        deque.append(root)
-        traversals = 0
-        while deque:
-            layer_size = len(deque)
-            layer = []
-            for i in range(layer_size):
-                front = deque.pop()
-                if front:
-                    layer.append(front.val)
-                    deque.appendleft(front.left)
-                    deque.appendleft(front.right)
-            if traversals % 2 != 0:
-                layer.reverse()
-            traversals += 1
-            if layer:
-                zigzag.append(layer)
-        
-        return zigzag
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        if not root:
+            return []
+
+        queue = collections.deque([root])
+        levels = []
+        while queue:
+            size = len(queue)
+            current_level = []
+            for _ in range(size):
+                # we check if it's even because when it is, we are appending the next odd level
+                if len(levels)%2 == 0: 
+                    front = queue.popleft()
+                    if front.left:
+                        queue.append(front.left)
+                    if front.right:
+                        queue.append(front.right)
+                else:
+                    front = queue.pop()
+                    if front.right:
+                        queue.appendleft(front.right)
+                    if front.left:
+                        queue.appendleft(front.left)
+                current_level.append(front.val)
+            levels.append(current_level)
+        return levels
